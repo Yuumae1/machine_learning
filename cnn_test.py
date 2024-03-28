671b2920ab9c2dca245415df099484ec4be4916d
Yuumae1 <maeda-y@aori.u-tokyo.ac.jp>
2024-03-28 13:40:56 +0900

    cnn_test.py

---
+++ cnn_test.py
@@ -0,0 +1,189 @@
+import numpy as np
+from numpy.linalg.linalg import norm
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization, LayerNormalization
+from keras.optimizers import Adam
+from keras.optimizers import RMSprop
+from keras.regularizers import l2
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+from matplotlib.markers import MarkerStyle
+#import cartopy.crs as ccrs
+#import cartopy.feature as cfeature
+import pandas as pd
+
+
+df1 = np.load('/home/maeda/archive/NOAA/olr_lanczosbp.npz')
+df2 = np.load('archive/ncep_reanalysis2/daily/wind/lanczosbp_w850.npz')
+
+lat = df1['lat']
+lon = df1['lon']
+olr = df1['bandpass_olr_jja'][80:-435,:,:]
+time = df1['time'][80:-435]
+real_time = pd.to_datetime(time, unit='h', origin=pd.Timestamp('1800-01-01')) # 時刻をdatetime型に変換
+print(lat.shape, lon.shape, olr.shape, time.shape, real_time.shape)
+print(real_time[0], real_time[-1])
+
+print(df2.files)
+u850 = df2['u_850'][80:-739,24:49,:]
+v850 = df2['v850'][80:-739,24:49,:]
+time_w = df2['time'][80:-739]
+real_time_w = pd.to_datetime(time_w, unit='h', origin=pd.Timestamp('1800-01-01'))
+print(u850.shape, v850.shape)
+print(real_time_w[0], real_time_w[-1])
+
+# 標準化処理
+print('=== Data Normalization ===')
+def normalization(data):
+  data_mean = np.mean(data, axis=0)
+  data_std  = np.std(data, axis=0)
+  data_norm = (data - data_mean) / data_std
+  print('Raw Data        = ', data.max(), data.min())
+  print('Normalized Data = ', data_norm.max(), data_norm.min())
+  del data_mean, data_std
+  return data_norm
+
+olr_norm = normalization(olr)
+u850_norm = normalization(u850)
+v850_norm = normalization(v850)
+
+# bsiso index (eEOF) 読み込み
+data_file = '/content/drive/MyDrive/bsiso_cnn/bsiso_filterd_index.dat'
+
+year    = np.loadtxt(data_file, usecols = 0, dtype = int)
+month   = np.loadtxt(data_file, usecols = 1, dtype = int)
+day     = np.loadtxt(data_file, usecols = 2, dtype = int)
+PC     = np.loadtxt(data_file, usecols = [3, 4])
+#PCy     = np.loadtxt(data_file, usecols = 4)
+phase   = np.loadtxt(data_file, usecols = 5, dtype = int)
+Amp_nrm_bsiso = np.loadtxt(data_file, usecols = 6, dtype = float)
+#index = np.loadtxt(data_file)
+
+print('month = ', month.shape)
+print('phase = ', phase.shape)
+print( 'PC = ', PC.shape)
+print('Amp_nrm_bsiso = ', Amp_nrm_bsiso.shape)
+print(year, month, day)
+
+# 全て一律にずらしたあと、インデクシングする
+lead_time = 0
+multi_forcast = False
+if multi_forcast == True:
+  output_shape = 2 * (lead_time + 1)
+else:
+  output_shape = 2
+print('output shape = ', output_shape)
+
+amp = Amp_nrm_bsiso[10:-lead_time-1]
+ph = phase[10:-lead_time-1]
+yy = year[10:-lead_time-1]
+mm = month[10:-lead_time-1]
+dd = day[10:-lead_time-1]
+rt = real_time[10:-lead_time-1]
+# 教師データは前進させる
+if multi_forcast == True:
+  sup_data = []
+  for j in range(lead_time+1):
+    sup_j = PC[10+j:-lead_time-1+j]
+    sup_data.append(sup_j)
+  sup_data = np.array(sup_data).transpose(1,0,2).reshape(-1, output_shape)
+else:
+  sup_data = PC[10+lead_time:]
+print(sup_data.shape)
+
+
+# 入力データの前処理
+def preprocess(data):
+  ipt_lag0  = data[10:-lead_time-1]
+  ipt_lag5  = data[5:-lead_time-6]
+  ipt_lag10 = data[:-lead_time-11]
+
+  # =========
+  # 訓練データの作成
+  idx = np.where((mm >= 5) & (mm <= 10) & (yy <= 2014))[0]
+  ipt_lag0_train = ipt_lag0[idx]
+  ipt_lag5_train = ipt_lag5[idx]
+  ipt_lag10_train = ipt_lag10[idx]
+  #ipt_train = np.concatenate([ipt_lag0_train, ipt_lag5_train, ipt_lag10_train], 1)
+  ipt_train = np.stack([ipt_lag0_train, ipt_lag5_train, ipt_lag10_train], 3)
+
+  # 検証データの作成
+  idx = np.where((mm >= 5) & (mm <= 10) & (yy > 2014))[0]
+  ipt_lag0_test = ipt_lag0[idx]
+  ipt_lag5_test = ipt_lag5[idx]
+  ipt_lag10_test = ipt_lag10[idx]
+  #ipt_test = np.concatenate([ipt_lag0_test, ipt_lag5_test, ipt_lag10_test], 1)
+  ipt_test = np.stack([ipt_lag0_test, ipt_lag5_test, ipt_lag10_test], 3)
+  return ipt_train, ipt_test
+
+olr_ipt_train, olr_ipt_test = preprocess(olr_norm)
+u850_ipt_train, u850_ipt_test = preprocess(u850_norm)
+v850_ipt_train, v850_ipt_test = preprocess(v850_norm)
+ipt_train = np.concatenate([olr_ipt_train, u850_ipt_train, v850_ipt_train], 3)
+ipt_test = np.concatenate([olr_ipt_test, u850_ipt_test, v850_ipt_test], 3)
+#ipt_train, ipt_test = v850_ipt_train, v850_ipt_test
+
+# その他のインデクシング
+idx = np.where((mm >= 5) & (mm <= 10) & (yy <= 2014))[0]
+sup_train = sup_data[idx]
+idx = np.where((mm >= 5) & (mm <= 10) & (yy > 2014))[0]
+sup_test = sup_data[idx]
+amp = amp[idx]
+ph = ph[idx]
+yy = yy[idx]
+mm = mm[idx]
+dd = dd[idx]
+rt = rt[idx]
+print(sup_test.shape, ipt_test.shape, ipt_train.shape, ph.shape, yy.shape, mm.shape, dd.shape)
+del olr_ipt_train, olr_ipt_test, u850_ipt_train, u850_ipt_test, v850_ipt_train, v850_ipt_test
+
+# CNNモデルの構築
+model = Sequential()
+# 入力画像　25×144×3 ：(緯度方向の格子点数)×(軽度方向の格子点数)×(チャンネル数、OLRのラグ)
+model.add(Conv2D(16, (3, 3), padding='same', input_shape=(25, 144, 9), strides=(2,2), ))   # ゼロパディング、バッチサイズ以外の画像の形状を指定 25*144*1 -> 25*144*8
+model.add(LayerNormalization())
+model.add(Activation('relu'))                                             # 活性化関数
+#model.add(MaxPooling2D(pool_size=(2, 2)))                                 # 21*140*16 -> 10*70*16
+model.add(Conv2D(32, (3, 3), padding='same', strides=(2,2)))                                             # 25*144*8 -> 21*140*16
+model.add(LayerNormalization())
+model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))                                 # 21*140*16 -> 10*70*16
+
+model.add(Conv2D(64, (2, 2), padding='same', strides=(2,2)))                             # 10*70*16 -> 10*70*32
+model.add(LayerNormalization())
+model.add(Activation('relu'))
+
+#model.add(MaxPooling2D(pool_size=(2, 2)))                                 # 10*70*32 -> 5*35*32
+#model.add(MaxPooling2D(pool_size=(2, 2)))                                 # 3*33*64 -> 1*16*64
+model.add(Dropout(0.2))  # ドロップアウト
+
+model.add(Flatten())  # 一次元の配列に変換                                # 1*16*64 -> 1024
+model.add(Dense(256))
+#model.add(Activation('relu'))
+#model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dense(output_shape, activation='linear'))
+
+model.summary()
+
+# モデルのコンパイルと学習の設定
+# !!! acuracy = 0.20を目指す !!!
+model.compile(optimizer=Adam(),
+              loss='mean_squared_error',  # MSE
+              )
+# モデルのトレーニング
+history = model.fit(ipt_train, sup_train, epochs=100, batch_size=128, validation_data=(ipt_test, sup_test))
+
+# モデルの出力を獲得する
+predict = model.predict(ipt_test, batch_size=None, verbose=0, steps=None)
+print(predict.shape)
+y_test = sup_test
+np.savez('/home/maeda/machine_learning/results/cnn-test' + str(lead_time) + 'day.npz', predict, y_test)
+
+print('=== Finish ===')
