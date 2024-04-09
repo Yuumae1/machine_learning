4fe1b863ab84c39264f62c6ff02d1c34d5039ea9
Yuumae1 <maeda-y@aori.u-tokyo.ac.jp>
2024-04-09 16:58:02 +0900

    del lag time

--- kikuchi_7val_2024-04-09.py
+++ kikuchi_7val_2024-04-09.py
@@ -102,26 +102,28 @@ print(sup_data.shape)
 
 # 入力データの前処理
 def preprocess(data):
-  ipt_lag0  = data[10:-lead_time-1]
-  ipt_lag5  = data[5:-lead_time-6]
-  ipt_lag10 = data[:-lead_time-11]
-
+  #ipt_lag0  = data[10:-lead_time-1]
+  #ipt_lag5  = data[5:-lead_time-6]
+  #ipt_lag10 = data[:-lead_time-11]
+  ipt = data[:-lead_time-1]
   # =========
   # 訓練データの作成(通年データとする)
   idx = np.where((yy <= 2014))[0]
-  ipt_lag0_train = ipt_lag0[idx]
-  ipt_lag5_train = ipt_lag5[idx]
-  ipt_lag10_train = ipt_lag10[idx]
+  #ipt_lag0_train = ipt_lag0[idx]
+  #ipt_lag5_train = ipt_lag5[idx]
+  #ipt_lag10_train = ipt_lag10[idx]
+  ipt_train = ipt[idx]
   #ipt_train = np.concatenate([ipt_lag0_train, ipt_lag5_train, ipt_lag10_train], 1)
-  ipt_train = np.stack([ipt_lag0_train, ipt_lag5_train, ipt_lag10_train], 3)
+  #ipt_train = np.stack([ipt_lag0_train, ipt_lag5_train, ipt_lag10_train], 3)
 
   # 検証データの作成
   idx = np.where((yy > 2014))[0]
-  ipt_lag0_test = ipt_lag0[idx]
-  ipt_lag5_test = ipt_lag5[idx]
-  ipt_lag10_test = ipt_lag10[idx]
+  #ipt_lag0_test = ipt_lag0[idx]
+  #ipt_lag5_test = ipt_lag5[idx]
+  #ipt_lag10_test = ipt_lag10[idx]
+  ipt_test = ipt[idx]
   #ipt_test = np.concatenate([ipt_lag0_test, ipt_lag5_test, ipt_lag10_test], 1)
-  ipt_test = np.stack([ipt_lag0_test, ipt_lag5_test, ipt_lag10_test], 3)
+  #ipt_test = np.stack([ipt_lag0_test, ipt_lag5_test, ipt_lag10_test], 3)
   return ipt_train, ipt_test
 
 olr_ipt_train, olr_ipt_test = preprocess(olr_norm)
@@ -132,8 +134,8 @@ v200_ipt_train, v200_ipt_test = preprocess(v200_norm)
 h850_ipt_train, h850_ipt_test = preprocess(h850_norm)
 pr_wtr_ipt_train, pr_wtr_ipt_test = preprocess(pr_wtr_norm)
 
-ipt_train = np.concatenate([olr_ipt_train, u850_ipt_train, v850_ipt_train, u200_ipt_train, v200_ipt_train, h850_ipt_train, pr_wtr_ipt_train], 3)
-ipt_test = np.concatenate([olr_ipt_test, u850_ipt_test, v850_ipt_test, u200_ipt_test, v200_ipt_test, h850_ipt_test, pr_wtr_ipt_test], 3)
+ipt_train = np.stack([olr_ipt_train, u850_ipt_train, v850_ipt_train, u200_ipt_train, v200_ipt_train, h850_ipt_train, pr_wtr_ipt_train], 3)
+ipt_test = np.stack([olr_ipt_test, u850_ipt_test, v850_ipt_test, u200_ipt_test, v200_ipt_test, h850_ipt_test, pr_wtr_ipt_test], 3)
 #ipt_train, ipt_test = v850_ipt_train, v850_ipt_test
 
 # その他のインデクシング
