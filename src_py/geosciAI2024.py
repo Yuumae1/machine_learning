9f056d00d6c029eab953910846e6ec6bd8f2e996
Yuumae1 <maeda-y@aori.u-tokyo.ac.jp>
2024-04-28 15:54:08 +0900

    l2 reg1e-4 & multistride:(3,3)

--- src_py/geosciAI2024.py
+++ src_py/geosciAI2024.py
@@ -95,19 +95,15 @@ ans_valid   = (ans_valid - ans_mean) / ans_std
 # CNNモデルの構築
 def cnn_model():
     model = Sequential()
-    model.add(Conv2D(32, (2, 2), padding='same', input_shape=(64, 64, 5), strides=(2,2)))   
+    model.add(Conv2D(32, (2, 2), padding='same', input_shape=(64, 64, 5), strides=(3,3), kernel_regularizer=l2(1e-4)))   
     model.add(BatchNormalization())
-    #model.add(LayerNormalization())
     model.add(Activation('relu'))                                                                             
     model.add(Conv2D(64, (2, 2), padding='same', strides=(2,2)))                                        
     model.add(BatchNormalization())
-    #model.add(LayerNormalization())
     model.add(Activation('relu'))                      
     model.add(Conv2D(128, (2, 2), padding='same', strides=(2,2)))                           
     model.add(BatchNormalization())
-    #model.add(LayerNormalization())
     #model.add(Conv2D(256, (2, 2), padding='same', strides=(2,2)))                           
-    #model.add(BatchNormalization())
     model.add(Activation('relu'))
     #model.add(Dropout(0.2))
 
