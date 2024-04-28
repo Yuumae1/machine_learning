e1fb2c978fdc4711a45302dfb373e7c248b9e529
Yuumae1 <maeda-y@aori.u-tokyo.ac.jp>
2024-04-28 17:44:47 +0900

    ans is only anomaly

--- src_py/geosciAI2024.py
+++ src_py/geosciAI2024.py
@@ -84,13 +84,14 @@ print('ans_valid   = ', ans_valid.shape)
 print('Normalization...')
 input_std  = np.mean(input_train, axis=0)
 input_mean = np.mean(input_train, axis=0)
-#ans_std    = np.std(ans_train, axis=0)
-#ans_mean   = np.std(ans_train, axis=0)
+ans_std    = np.std(ans_train, axis=0)
+ans_mean   = np.std(ans_train, axis=0)
 
 input_train = (input_train - input_mean) / input_std
 input_valid = (input_valid - input_mean) / input_std
-#ans_train   = (ans_train - ans_mean) / ans_std
-#ans_valid   = (ans_valid - ans_mean) / ans_std
+ans_train   = (ans_train - ans_mean) #/ ans_std  偏差で処理する（計算上ではRMSEは同じ）
+ans_valid   = (ans_valid - ans_mean) #/ ans_std
+print('ans_mean, ans_std = ', ans_mean, ans_std)
 
 # CNNモデルの構築
 def cnn_model():
