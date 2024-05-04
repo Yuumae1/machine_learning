865dd4c8ab3ac9393bc9d2063722aefd7231c8e3
Yuumae1 <maeda-y@aori.u-tokyo.ac.jp>
2024-05-04 22:30:24 +0900

    modify

--- src_py/geosciAI2024.py
+++ src_py/geosciAI2024.py
@@ -86,7 +86,7 @@ def get_input_ans(start_year, end_year, input_dir, n_input = 1):
                     ns_sst = "n"
             # 対応するデータの読み込み
             for jj in range(len(field)):
-                if field[jj] == 'sst':   # SST の読み込み
+                if jj == 2:   # SST の読み込み
                     round_lon_sst = Decimal(lon_sst).quantize(Decimal('0'), rounding=ROUND_HALF_UP)
                     round_lat_sst = Decimal(lat_sst).quantize(Decimal('0'), rounding=ROUND_HALF_UP)
                     if round_lon_sst == 360:
