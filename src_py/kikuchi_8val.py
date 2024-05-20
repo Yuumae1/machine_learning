68a9bffc98db51717e48f9253ba3dfb97a9eeb1d
Yuumae1 <maeda-y@aori.u-tokyo.ac.jp>
2024-05-20 21:40:23 +0900

    lt = 35~40

--- src_py/kikuchi_8val.py
+++ src_py/kikuchi_8val.py
@@ -153,7 +153,7 @@ def learning_curve(history, lead_time):
 
 # ==== iteration program ====
 #lt_box = [5, 10, 15, 20, 25, 30, 35]
-lt_box = np.arange(41)
+lt_box = np.arange(35,41)
 for lead_time in lt_box:
 
   print('==== lead time : {} day ====='.format(lead_time))
