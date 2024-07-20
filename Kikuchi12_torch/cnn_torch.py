f2956073adef3b5d58fd8945d81e4ba71f3a4340
Yuumae1 <maeda-y@aori.u-tokyo.ac.jp>
2024-07-20 15:50:09 +0900

    seed == 1 only

--- Kikuchi12_torch/cnn_torch.py
+++ Kikuchi12_torch/cnn_torch.py
@@ -198,7 +198,7 @@ if __name__ == '__main__':
     n_batches = x_train.shape[0] // batch_size
     epoch_num = 200
     
-    for seed in range(20):
+    for seed in range(1):
       print('Seed = ', seed)
       set_seed(seed)
       model = Conv().to(device)
