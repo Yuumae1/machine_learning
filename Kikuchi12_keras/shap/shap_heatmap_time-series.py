b81609ebbd80b7876b9432065fa6491f0055ccb9
Yuumae1 <maeda-y@aori.u-tokyo.ac.jp>
2024-08-25 16:28:52 +0900

    analysis

--- Kikuchi12_keras/shap/shap_heatmap_time-series.py
+++ Kikuchi12_keras/shap/shap_heatmap_time-series.py
@@ -21,87 +21,72 @@ def main(tt, pcs):
     df3 = np.load(f'/work/gi55/i55233/data/machine_learning/results/kikuchi-8vals_v1/cor/8vals/000day/seed{(seed[0]):03}.npz')
 
     shap_data = df['shap_values']
-    pred = df2['arr_0'][:-1-(lt_max-tt)] # 対応する pred の値
-    initial = df3['arr_0'][:-1-(lt_max)] # 初期値
-    #print(shap_data.shape)
-    #print(pred.shape)
-    #print(initial.shape)
-    
+    pred = df2['arr_0'][tt:-1-lt_max] # 対応する pred の値
+    initial = df3['arr_0'][:-1-lt_max] # 初期値
     # shap の絶対値の寄与度を計算
-    shap_abs = np.abs(shap_data.sum(axis=(2,3)))
+    #shap_abs = np.abs(shap_data.sum(axis=(2,3)))
+    shap_abs = shap_data.sum(axis=(2,3))
     
     shap_val_abs = np.zeros((shap_abs.shape[0], shap_abs.shape[1], 8))
     for j in range(8):
         shap_val_abs[:,:,j] = shap_abs[:,:,j*3:(j+1)*3].sum(axis=2)
     
+    print(shap_val_abs.shape) # (2, 644, 8)
     
     # 20150101-20231231 のデータを取得
     sup_rt = pd.date_range('2016-01-01', '2022-12-31', freq='D')
-    jja = np.where((sup_rt.year > 2015) & (sup_rt.month >= 6) & (sup_rt.month <= 8))[0]    
-    pred_jja = pred[jja]
-    init_jja = initial[jja]
-    #print(pred.shape)
-    #print(pred_jja.shape, init_jja.shape)
-    
-    # skill of phase representation
-    bin_x = np.arange(-3, 3.1, 0.5)
-    bin_y = np.arange(-3, 3.1, 0.5)
+    sup_rt_jja = sup_rt[np.where((sup_rt.month >= 6) & (sup_rt.month <= 8))]
+    for yy in range(2016, 2023):
+        yy_jja = np.where((sup_rt.year == yy) & (sup_rt.month >= 6) & (sup_rt.month <= 8))[0]    
+        pred_jja = pred[yy_jja]
+        init_jja = initial[yy_jja]
+        rt_yy = sup_rt[yy_jja]
+        #print(rt_yy.shape, pred_jja.shape, init_jja.shape, sup_rt.shape)
+        # shap のjja は要素数が異なるため別にインデクシングする
+        jja = np.where((sup_rt_jja.year == yy))[0]
+        
+        
+        # 3 * 3 のフェーズ分布を描画
+        val = ['OLR', 'U850', 'U200', 'V850', 'V200', 'H850', 'PW', 'SST']
+        fig = plt.figure(figsize=(16,6))
+        ax1 = fig.add_subplot(2, 1, 1)
+        # 高さの占める割合を調整する
+        ax1.set_aspect(0.8)
+        ax1.plot(rt_yy, pred_jja[:,pcs], color='g')
+        ax1.set_xlim(rt_yy[0], rt_yy[-1])
+        ax1.set_ylim(-3, 3)
+        #ax1.set_yticks(np.arange(-3, 3.1, -1.5))
+        ax1.hlines(0, rt_yy[0], rt_yy[-1], 'k', linestyle='dashed', linewidth=0.3)
+        ax2 = fig.add_subplot(2, 1, 2)   
+        # アスペクト比を 4:1
+        #ax2.set_aspect(4)
+        xi, yi = np.meshgrid(np.arange(92), np.arange(8))
+        pcm = ax2.pcolormesh(xi, yi, shap_val_abs[pcs,jja].T, cmap='seismic', vmin=-0.6, vmax=0.6)
 
-    #print(bin_x)
-    # bin ごとの平均寄与度を計算
-    shap_bin = np.zeros((len(bin_x)-1, len(bin_y)-1, 8))
-    for i in range(len(bin_x)-1):
-        for j in range(len(bin_y)-1):
-            idx = np.where((pred_jja[:,0] >= bin_x[i]) & (pred_jja[:,0] < bin_x[i+1]) & (pred_jja[:,1] >= bin_y[j]) & (pred_jja[:,1] < bin_y[j+1]))[0]
-            if len(idx) > 0:
-                shap_bin[i,j] = shap_val_abs[pcs,idx].mean(axis=0)
-            else:
-                shap_bin[i,j] = np.nan
-    
-    cnt_rate = shap_bin / shap_bin.sum(axis=2)[:,:,np.newaxis]
-    
-    # 3 * 3 のフェーズ分布を描画
-    val = ['OLR', 'U850', 'U200', 'V850', 'V200', 'H850', 'PW', 'SST']
-    fig = plt.figure(figsize=(16,16))
-    for i in range(8):
-        ax = fig.add_subplot(3,3,i+1)
-        # アスペクト比を 1:1 に
-        ax.set_aspect('equal')
-        # 間隔を開ける
-        # r = 1 の円
-        theta = np.linspace(0, 2*np.pi, 100)
-        x = np.cos(theta)
-        y = np.sin(theta)
-        ax.plot(x, y, 'k', linewidth=0.5)
-        # 中心線
-        ax.hlines(0, -3, 3, 'k', linewidth=0.5)
-        ax.vlines(0, -3, 3, 'k', linewidth=0.5)
-        # 斜線
-        ax.plot([-3, -np.sqrt(2)/2], [-3, -np.sqrt(2)/2], 'k', linewidth=0.5)
-        ax.plot([np.sqrt(2)/2, 3], [np.sqrt(2)/2, 3], 'k', linewidth=0.5)
-        ax.plot([-3, -np.sqrt(2)/2], [3, np.sqrt(2)/2], 'k', linewidth=0.5)
-        ax.plot([np.sqrt(2)/2, 3], [-np.sqrt(2)/2, -3], 'k', linewidth=0.5)
+        ax2.set_xlabel('Time', fontsize=15)
 
-        xi, yi = np.meshgrid(bin_x, bin_y)
-        pcm = ax.pcolormesh(xi, yi, cnt_rate[:,:,i], cmap='YlOrRd', vmin=0, vmax=0.3)
-
-        ax.set_xlabel('-PC1', fontsize=15)
-        ax.set_ylabel('-PC2', fontsize=15)
-        ax.set_xticks(np.arange(3, -3, -1))
-        ax.set_yticks(np.arange(3, -3, -1))
-        ax.text(-2.5, 2.5, val[i], fontsize=15)
-
-        ax.set_xlim(-3, 3)
-        ax.set_ylim(-3, 3)
-    cbar_ax = fig.add_axes([0.925, 0.3, 0.02, 0.4]) # [左からの位置, 下からの位置, 幅, 高さ]
-    fig.colorbar(pcm, cax=cbar_ax)  
-    # save
-    plt.savefig(f'/work/gi55/i55233/data/results/bsiso_shap/ph_cnt/phase_rep_{tt:03}_pc{pcs}.png', bbox_inches='tight')
-
+        ax2.set_xlim(0, 91)
+        ax2.set_ylim(7.5, -0.5)
+        # y軸に text を表示
+        ax2.set_yticks(np.arange(8))
+        ax2.set_yticklabels(val, fontsize=15)
+        # plot の幅を揃える
+        fig.canvas.draw()
+        axpos1 = ax1.get_position() # 上の図の描画領域
+        axpos2 = ax2.get_position() # 下の図の描画領域
+        #幅をax1と同じにする
+        ax2.set_position([axpos2.x0, axpos2.y0, axpos1.width, axpos2.height+0.12]) # [左からの位置, 下からの位置, 幅, 高さ]
+        cbar_ax = fig.add_axes([0.925, 0.12, 0.015, 0.43]) # [左からの位置, 下からの位置, 幅, 高さ]
+        plt.colorbar(pcm, cax=cbar_ax)
+        title = f'PC{pcs+1} SHAP values (Lead time = {tt} day)      year = {yy}'
+        fig.suptitle(title, fontsize=20, y=0.83)
+        # save
+        plt.savefig(f'/work/gi55/i55233/data/results/bsiso_shap/heatmap_tmseries/{(tt):03}/pc{pcs+1}_{yy}.png', bbox_inches='tight', dpi=200)
+        plt.close()
 
 if __name__ == '__main__':
     
-    for tt in range(0,36):
+    for tt in [5, 10, 15, 20, 25, 30]:
         for pcs in range(2):
             main(tt, pcs)
     
